b6dc78df5155044eaf70e4e2dc5a623d8ec0c385
k-p25 <kevinp7@illinois.edu>
2025-06-26 15:49:34 -0500

    day 19-more with turtle graphics

---
+++ day-19/etch-a-sketch.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+def move_backwards():
+    tim.backward(10)
+def counter_clockwise():
+    tim.left(5)
+def clockwise():
+    tim.right(5)
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=counter_clockwise)
+screen.onkey(key="d", fun=clockwise)
+screen.onkey(key="c", fun=clear)
+
+screen.exitonclick()
